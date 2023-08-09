70e5ff2a6f7ac7d8f8834153967eaf993b3832fd
Jason Li <jasonli99193@gmail.com>
2023-08-09 10:43:54 -0500

    create post

--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import json
-from views import create_user, login_user, get_all_posts, get_single_post, get_all_categories, get_all_tags
+from views import create_user, login_user, get_all_posts, get_single_post, create_post, get_all_categories, get_all_tags
 
 
 
